9ee3988edbb68e989b58defd61f87c678368395f
santhoshkumarr231299 <santhosh.kumarr231299@gmail.com>
2024-09-15 01:41:34 +0530

    [santhosh] Add. chat server

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 git clone https://github.com/santhoshkumarr231299/lifeeasy-client.git
 git clone https://github.com/santhoshkumarr231299/lifeeasy-server.git
+git clone https://github.com/santhoshkumarr231299/lifeeasy-chat-server.git
